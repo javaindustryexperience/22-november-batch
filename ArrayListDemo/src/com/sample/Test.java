6a95539fcf78056d326662325d5f6f37376ec3df
Jeevan <javaindustryexperience@gmail.com>
2026-02-06 21:52:17 +0530

    added java features line

--- ArrayListDemo/src/com/sample/Test.java
+++ ArrayListDemo/src/com/sample/Test.java
@@ -5,5 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("This is Java Full Stack Development Batch..");
+		System.out.println("This is java features");
 	}
 }
