ce91f5acd32034bd442ece55054cf687efe1acab
Jeevan <javaindustryexperience@gmail.com>
2026-02-06 21:29:32 +0530

    added arraylist city code

---
+++ ArrayListDemo/src/com/sample/ArrayListDemo1.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo1 {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("pune");
+		al.add("mumbai");
+		al.add("banglore");
+		al.add("delhi");
+		System.out.println(al);
+	}
+}
